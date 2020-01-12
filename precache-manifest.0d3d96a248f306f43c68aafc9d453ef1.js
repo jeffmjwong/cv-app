cc05b8b54830f811e9f5c940d3b6aa34c13acea1
Jeff Wong <jeffmjwong@gmail.com>
2020-01-12 18:14:49 +1000

    Updates

---
+++ precache-manifest.0d3d96a248f306f43c68aafc9d453ef1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ce3996bfc06b134eec248b743dd61e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "b62217376ce7eb7595ec",
+    "url": "/react-gh-pages/static/css/2.9afb3899.chunk.css"
+  },
+  {
+    "revision": "388af97b72eb25ae540c",
+    "url": "/react-gh-pages/static/css/main.8c936c1f.chunk.css"
+  },
+  {
+    "revision": "b62217376ce7eb7595ec",
+    "url": "/react-gh-pages/static/js/2.b3bbc2ac.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-gh-pages/static/js/2.b3bbc2ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "388af97b72eb25ae540c",
+    "url": "/react-gh-pages/static/js/main.29899490.chunk.js"
+  },
+  {
+    "revision": "5f18237001f2f198d1cb",
+    "url": "/react-gh-pages/static/js/runtime-main.3c49a3b2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/react-gh-pages/static/media/open-sans-v17-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/react-gh-pages/static/media/open-sans-v17-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/react-gh-pages/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/react-gh-pages/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  },
+  {
+    "revision": "ef50a98a9a6c92f872f44ecdec584b28",
+    "url": "/react-gh-pages/static/media/pic.ef50a98a.jpg"
+  }
+]);
